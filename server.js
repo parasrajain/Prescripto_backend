9ea9729a604b63ded0f80a51a7bf7e6c48bf2a7f
parasrajain <parasrajain7@gmail.com>
2025-02-04 21:04:37 +0530

    test

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config
+const app=express()
+const port =process.env.PORT || 4000;
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+// api end point
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port,()=>{
+    console.log("Server started at",port)
+})
